91cc69e60a118f7edd5f1fd737dc0ccc33740bd0
HarshSahuIIITS <harshsahuu162@gmail.com>
2025-08-28 18:50:59 +0530

    Create useFetch.ts

---
+++ services/useFetch.ts
@@ -0,0 +1,40 @@
+// fetch movies
+// fetch movie details
+
+import {useEffect, useState} from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T> , autoFetch = true) => {
+    const[data, setData] = useState<T | null>(null);
+    const[loading, setLoading] = useState(false);
+    const[error, setError] = useState<Error | null>(null);
+
+
+    const fetchData = async () => {
+        try{
+            setLoading(true);
+            setError(null);
+            const result = await fetchFunction();
+            setData(result);
+        }
+        catch(err){
+            setError(err instanceof Error ? err : new Error('New error occured'));
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    const reset = async () => {
+        setData(null);
+        setLoading(false);
+        setError(null);
+    }
+
+    useEffect(() => {
+        if(autoFetch){
+            fetchData();
+        }
+    }, []);
+    return {data,loading,error, refetch: fetchData, reset};
+}
+export default useFetch;
